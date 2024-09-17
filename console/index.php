9c767a6bad2326d8d8ab0d4741377c0bb44ebdee
GioSF <giosfdev@gmail.com>
2024-09-17 20:52:22 -0300

    Update env variables use

--- console/index.php
+++ console/index.php
@@ -5,8 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-defined('YII_DEBUG') or define('YII_DEBUG', filter_var(getenv('YII_DEBUG'), FILTER_VALIDATE_BOOLEAN));
-defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV') ?: 'prod');
+defined('YII_DEBUG') or define('YII_DEBUG', filter_var($_ENV['YII_DEBUG'], FILTER_VALIDATE_BOOLEAN));
+defined('YII_ENV') or define('YII_ENV', $_ENV['YII_ENV'] ?: 'prod');
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
